55caffa16e9dd076c44cfe7834497d10758596ac
Jordan Marine <marine.jordan64@gmail.com>
2025-06-14 01:19:49 -0500

    Forgot I didn't need that anymore...

--- lua/terrortown/autorun/server/sv_ballot_hotfix.lua
+++ lua/terrortown/autorun/server/sv_ballot_hotfix.lua
@@ -1,5 +1,3 @@
-local oldCollectMaps = MapVotePools.CollectMaps
-MapVotePools.CollectMaps = function(prefix, current, limit) MapVotePools.CurrentMaps = oldCollectMaps(prefix, current, limit) end
 MapVotePools.Ballot.HandleClientBallot = function(ply)
     net.Start("MVP_MapVotePoolsBallot")
     net.Send(ply)
