17e808812ef418127b564bd71270ee7401542835
Jordan Marine <marine.jordan64@gmail.com>
2025-06-14 05:10:27 -0500

    Fixed icons and made sharks and minnows toggle logic better.

--- lua/terrortown/autorun/server/sv_sharks_minnows.lua
+++ lua/terrortown/autorun/server/sv_sharks_minnows.lua
@@ -1,4 +1,24 @@
 CreateConVar("ttt2_sharks_minnows", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+cvars.AddChangeCallback("ttt2_sharks_minnows", function(convarName, oldValue, newValue)
+    if newValue == 0 then
+        GetConVar("ttt2_max_melee_slots"):SetInt(1)
+        GetConVar("ttt2_max_secondary_slots"):SetInt(0)
+        GetConVar("ttt2_max_primary_slots"):SetInt(0)
+        GetConVar("ttt2_max_nade_slots"):SetInt(0)
+        GetConVar("ttt2_max_carry_slots"):SetInt(0)
+        GetConVar("ttt2_max_unarmed_slots"):SetInt(1)
+        GetConVar("ttt2_max_special_slots"):SetInt(0)
+    else
+        GetConVar("ttt2_max_melee_slots"):SetInt(1)
+        GetConVar("ttt2_max_secondary_slots"):SetInt(1)
+        GetConVar("ttt2_max_primary_slots"):SetInt(1)
+        GetConVar("ttt2_max_nade_slots"):SetInt(1)
+        GetConVar("ttt2_max_carry_slots"):SetInt(1)
+        GetConVar("ttt2_max_unarmed_slots"):SetInt(1)
+        GetConVar("ttt2_max_special_slots"):SetInt(2)
+    end
+end)
+
 hook.Add("TTT2ModifyFinalRoles", "MURKSharksMinnows", function(finalRoles)
     if not GetConVar("ttt2_sharks_minnows"):GetBool() then return end
     local traitor_roles = {}
@@ -18,3 +38,12 @@ hook.Add("TTT2ModifyFinalRoles", "MURKSharksMinnows", function(finalRoles)
         end
     end
 end)
+
+hook.Add("TTTBeginRound", "MURK_SMRemoveCrowbar", function()
+    GetConVar("ttt2_max_melee_slots"):SetInt(0)
+    for _, ply in pairs(player.GetAll()) do
+        if ply:GetSubRole() == ROLE_MINNOW then ply:RemoveEquipmentWeapon("weapon_zm_improvised") end
+    end
+end)
+
+hook.Add("TTTEndRound", "MURK_SMResetLoadout", function() GetConVar("ttt2_max_melee_slots"):SetInt(1) end)
